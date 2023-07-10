c54b5a1ecc9e473d613c57e1c8b8cfc29b8b17cd
poonawitch <59003699+poonawitch@users.noreply.github.com>
2023-07-10 17:38:27 +0700

    fix: cronjob.sh

--- install_cronjob.sh
+++ install_cronjob.sh
@@ -19,36 +19,35 @@ echo -e "API_URL: \e[1m\e[32m$API_URL\e[0m"
 echo '================================================='
 sleep 3
 
-echo -e "\e[1m\e[32m1. Installing $NAME Cronjob with $FREQUENCY ms frequency... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Installing $NAME Cronjob with $FREQUENCY ms frequency...\e[0m" && sleep 1
 # install cosmos-exporter
-wget https://github.com/I3acon/animated-sniffle/releases/download/Cronjob-v.1/cronjob
+wget -O cronjob https://github.com/I3acon/animated-sniffle/releases/download/Cronjob-v.1/cronjob
 chmod +x cronjob
 
 export USERNAME=$(whoami)
-sudo -E bash -c 'cat << EOF > /etc/systemd/system/$NAME.service
+sudo -E bash -c "cat << EOF > /etc/systemd/system/$NAME.service
 [Unit]
 Description=$NAME Daemon
 After=network-online.target
 
 [Service]
 User=$USERNAME
-ExecStart=./$HOME/cronjob $NODE_URL $VAL_ADR $BLOCK_EXPLORER $THRESHOLD $FREQUENCY $API_URL
+ExecStart=$HOME/cronjob $NODE_URL $VAL_ADR $BLOCK_EXPLORER $THRESHOLD $FREQUENCY $API_URL
 Restart=always
 RestartSec=3
 LimitNOFILE=4096
 
 [Install]
 WantedBy=multi-user.target
-EOF'
-
+EOF"
 
 sudo systemctl daemon-reload
 sudo systemctl enable $NAME
 sudo systemctl start $NAME
 
-echo -e "\e[1m\e[32mInstallation finished... \e[0m" && sleep 1
-echo -e "\e[1m\e[32m2. Checking $NAME status... \e[0m" && sleep 1
+echo -e "\e[1m\e[32mInstallation finished...\e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Checking $NAME status...\e[0m" && sleep 1
 sudo systemctl status $NAME
 
-echo -e "\e[1m\e[32m3. Checking $NAME logs... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Checking $NAME logs...\e[0m" && sleep 1
 sudo journalctl -u $NAME -f
